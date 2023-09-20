f386d330830f25a7eb7a74540b6545e84f80615c
Chris <117781333+chrism-la@users.noreply.github.com>
2023-09-20 01:20:05 -0700

    changed to dblclick

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-function newImage(source, leftValue, bottomValue) {
+function newImage(imageFile, leftValue, bottomValue) {
    let newImage = document.createElement("img");
-   newImage.src = source;
+   newImage.src = imageFile;
    newImage.style.position = "fixed";
    newImage.style.left = leftValue;
    newImage.style.bottom = bottomValue;
@@ -11,18 +11,18 @@ newImage("assets/green-character.gif", "100px", "100px");
 newImage("assets/pine-tree.png", "450px", "200px");
 newImage("assets/tree.png", "200px", "300px");
 newImage("assets/pillar.png", "350px", "100px");
-newImage("assets./crate.png", "150px", "200px");
+newImage("assets/crate.png", "150px", "200px");
 newImage("assets/well.png", "500px", "425px");
 
-function newItem(source, leftValue, bottomValue) {
+function newItem(imageFile, leftValue, bottomValue) {
    let newItem = document.createElement("img");
-   newItem.src = source;
+   newItem.src = imageFile;
    newItem.style.position = "fixed";
    newItem.style.left = leftValue;
    newItem.style.bottom = bottomValue;
    document.body.append(newItem);
 
-   newItem.addEventListener("click", function () {
+   newItem.addEventListener("dblclick", function () {
       newItem.remove();
    });
 }
