834479f64dda9e2f6a36c04f8268c293c8a303d9
Chris <117781333+chrism-la@users.noreply.github.com>
2023-09-20 00:57:02 -0700

    passed all other images through function

--- index.js
+++ index.js
@@ -9,3 +9,7 @@ function newImage(source, leftValue, bottomValue) {
 
 newImage("assets/green-character.gif", "100px", "100px");
 newImage("assets/pine-tree.png", "450px", "200px");
+newImage("assets/tree.png", "200px", "300px");
+newImage("assets/pillar.png", "350px", "100px");
+newImage("assets./crate.png", "150px", "200px");
+newImage("assets/well.png", "500px", "425px");
