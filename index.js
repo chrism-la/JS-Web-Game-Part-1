80c91e55d388bbb42f5a1bc8013b6ed4615e2690
Chris <117781333+chrism-la@users.noreply.github.com>
2023-09-20 00:54:40 -0700

    created the newImage function with parameters labeled as Source bottomValue and leftValue. then passed the greenCharacter and pineTree arguments through the function

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
-let greenCharacter = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter)
+function newImage(source, leftValue, bottomValue) {
+   let newImage = document.createElement("img");
+   newImage.src = source;
+   newImage.style.position = "fixed";
+   newImage.style.left = leftValue;
+   newImage.style.bottom = bottomValue;
+   document.body.append(newImage);
+}
+
+newImage("assets/green-character.gif", "100px", "100px");
+newImage("assets/pine-tree.png", "450px", "200px");
